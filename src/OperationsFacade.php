f7b76e7063f99a239e80bd181c5abdb01445c27c
satahippy <satahippy@gmail.com>
2015-05-31 20:44:14 +0300

    basic facade for executing dbunit operations

---
+++ src/OperationsFacade.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Sata\DbTest;
+
+use PDO;
+use PHPUnit_Extensions_Database_DataSet_ArrayDataSet;
+use PHPUnit_Extensions_Database_DataSet_IDataSet;
+use PHPUnit_Extensions_Database_DB_DefaultDatabaseConnection;
+use PHPUnit_Extensions_Database_DB_IDatabaseConnection;
+use PHPUnit_Extensions_Database_Operation_IDatabaseOperation;
+
+class OperationsFacade
+{
+    /**
+     * @var PDO
+     */
+    protected $pdo;
+
+    /**
+     * @var PHPUnit_Extensions_Database_DB_IDatabaseConnection
+     */
+    protected $dbUnitConnection;
+
+    /**
+     * @param PDO $pdo
+     */
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * @return PDO
+     */
+    public function getPDO()
+    {
+        return $this->pdo;
+    }
+
+    /**
+     * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
+     */
+    protected function getDBUnitConnection()
+    {
+        if ($this->dbUnitConnection === null) {
+            $this->dbUnitConnection = $this->createDBUnitConnection();
+        }
+        return $this->dbUnitConnection;
+    }
+
+    /**
+     * @param string $operation
+     * @param mixed $data
+     */
+    public function executeOperation($operation, $data)
+    {
+        $operation = $this->createDBUnitOperation($operation);
+        $data = $this->createDBUnitDataSet($data);
+        $connection = $this->getDBUnitConnection();
+        $operation->execute($connection, $data);
+    }
+
+    /**
+     * @param string $operation
+     * 
+     * @return PHPUnit_Extensions_Database_Operation_IDatabaseOperation
+     */
+    protected function createDBUnitOperation($operation)
+    {
+        switch ($operation) {
+            case 'none':
+            case 'clean_insert':
+            case 'insert':
+            case 'truncate':
+            case 'delete':
+            case 'delete_all':
+            case 'update':
+                return call_user_func(['PHPUnit_Extensions_Database_Operation_Factory', strtoupper($operation)]);
+        }
+    }
+
+    /**
+     * @param $data
+     * 
+     * @return PHPUnit_Extensions_Database_DataSet_IDataSet
+     */
+    protected function createDBUnitDataSet($data)
+    {
+        return new PHPUnit_Extensions_Database_DataSet_ArrayDataSet($data);
+    }
+
+    /**
+     * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
+     */
+    protected function createDBUnitConnection()
+    {
+        return new PHPUnit_Extensions_Database_DB_DefaultDatabaseConnection($this->pdo);
+    }
+}
---
+++ tests/Unit/OperationsFacadeTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Sata\DbTest\Test\Unit;
+
+use PDO;
+use PHPUnit_Extensions_Database_DataSet_IDataSet;
+use PHPUnit_Extensions_Database_DB_IDatabaseConnection;
+use PHPUnit_Extensions_Database_Operation_IDatabaseOperation;
+use PHPUnit_Framework_TestCase;
+use Sata\DbTest\OperationsFacade;
+
+class OperationsFacadeTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var PDO
+     */
+    protected $pdo;
+
+    /**
+     * @var PHPUnit_Extensions_Database_Operation_IDatabaseOperation
+     */
+    protected $operation;
+
+    /**
+     * @var PHPUnit_Extensions_Database_DB_IDatabaseConnection
+     */
+    protected $connection;
+
+    /**
+     * @var PHPUnit_Extensions_Database_DataSet_IDataSet
+     */
+    protected $dataSet;
+    
+    public function setUp()
+    {
+        $this->pdo = $this->getMock('\Sata\DbTest\Test\PDOMock');
+        $this->operation = $this->getMock('\PHPUnit_Extensions_Database_Operation_IDatabaseOperation');
+        $this->connection = $this->getMock('\PHPUnit_Extensions_Database_DB_IDatabaseConnection');
+        $this->dataSet = $this->getMock('\PHPUnit_Extensions_Database_DataSet_IDataSet');
+    }
+
+    /**
+     * @param array $mockMethods
+     * 
+     * @return OperationsFacade
+     */
+    protected function getOperationFacade($mockMethods = ['createDBUnitOperation', 'createDBUnitConnection', 'createDBUnitDataSet'])
+    {
+        $facade = $this->getMockBuilder('\Sata\DbTest\OperationsFacade')
+            ->setConstructorArgs([$this->pdo])
+            ->setMethods($mockMethods)
+            ->getMock();
+        
+        foreach ($mockMethods as $method) {
+            switch ($method) {
+                case 'createDBUnitOperation':
+                    $facade
+                        ->expects($this->any())
+                        ->method('createDBUnitOperation')
+                        ->willReturn($this->operation);
+                    break;
+                case 'createDBUnitConnection':
+                    $facade
+                        ->expects($this->any())
+                        ->method('createDBUnitConnection')
+                        ->willReturn($this->connection);
+                    break;
+                case 'createDBUnitDataSet':
+                    $facade
+                        ->expects($this->any())
+                        ->method('createDBUnitDataSet')
+                        ->willReturn($this->dataSet);
+                    break;
+            }
+        }
+        
+        return $facade;
+    }
+
+    public function testExecuteOperation()
+    {
+        $this->operation
+            ->expects($this->once())
+            ->method('execute');
+        
+        $facade = $this->getOperationFacade();
+        $facade->expects($this->once())
+            ->method('createDBUnitOperation')
+            ->with('insert');
+        $facade->expects($this->once())
+            ->method('createDBUnitDataSet')
+            ->with(['test data']);
+        
+        $facade->executeOperation('insert', ['test data']);
+    }
+}
